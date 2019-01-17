c8d8c7579ac37852442a5af97e642fcc2986838f
Karen Kepner <kkepner14 (at) gmail.com>
2019-01-17 13:47:52 -0700

    add event listener for card click

--- js/app.js
+++ js/app.js
@@ -1,18 +1,38 @@
-//16 pairs of cards to match
-let cards = [
+//16 pairs of card icons to match
+let cardIcon = [
   'fa-coffee', 'fa-coffee', 'fa-dog', 'fa-dog', 'fa-crow', 'fa-crow', 'fa-dragon', 'fa-dragon', 'fa-fire', 'fa-fire', 'fa-hippo', 'fa-hippo', 'fa-democrat', 'fa-democrat', 'fa-snowflake', 'fa-snowflake'
 ]
-let board = document.getElementById('game-board')
+let board = document.getElementById('game-board');
 
-
-function generateCards(card) {
-  return `<div class="card col3"><i class="fa ${card}"></i></div>`;
+function generateCards(icon) {
+  return `<div class="card col3"><i class="fa ${icon}"></i></div>`;
 }
 
 function startGame() {
-  let deck = shuffle(cards).map(function(cards) {return generateCards(cards)});
+  let deck = shuffle(cardIcon).map(function(cardIcon) {return generateCards(cardIcon)});
   board.innerHTML = deck.join('');
+  clickCards();
 }
+
+//add click event to cards, use event bubbling
+//click event flips over card (shows icon)
+
+function clickCards() {
+  board.addEventListener('click', function(event) {
+    if (event.target.classList.contains('card')) {
+      event.target.classList.add('show');
+    };
+  })
+}
+//compare cards
+//if they match, assign match class
+//if they don't match, hide them again after some animation and a few seconds
+//only allow two cards to be shown at once
+//make all the boxes stay the same size
+//add a timer for the game
+//add the star rating in the scoreboard
+//count the moves and add to scoreboard
+//add modal when the game is won showing the score and star rating
 
 startGame()
 //shuffle and place cards on pageload
