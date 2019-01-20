12ce9804d2d17fe78db063af3e3a5403f62fa2dd
Karen Kepner <kkepner14 (at) gmail.com>
2019-01-20 14:45:00 -0700

    compare cards with timeout

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@ let cardIcon = [
   'fa-coffee', 'fa-coffee', 'fa-dog', 'fa-dog', 'fa-crow', 'fa-crow', 'fa-dragon', 'fa-dragon', 'fa-fire', 'fa-fire', 'fa-hippo', 'fa-hippo', 'fa-democrat', 'fa-democrat', 'fa-snowflake', 'fa-snowflake'
 ]
 let board = document.getElementById('game-board');
+let pickedCards = [];
+let moves = 0;
 
 function generateCards(icon) {
-  return `<div class="card col3"><i class="fa ${icon}"></i></div>`;
+  return `<div class="card col3" data-card=${icon}><i class="fa ${icon}"></i></div>`;
 }
 
 function startGame() {
@@ -14,17 +16,42 @@ function startGame() {
   clickCards();
 }
 
-//add click event to cards, use event bubbling
-//click event flips over card (shows icon)
-
 function clickCards() {
   board.addEventListener('click', function(event) {
-    if (event.target.classList.contains('card')) {
-      event.target.classList.add('show');
-    };
+    let et = event.target;
+    if (et.classList.contains('card') && !et.classList.contains('open') && !et.classList.contains('show') && !et.classList.contains('match') && pickedCards.length < 2) {
+      pickedCards.push(et);
+      et.classList.add('show', 'open');
+    }
+    compareCards(pickedCards);
+    
   })
 }
+
 //compare cards
+function compareCards(cards) {
+  if (pickedCards.length === 2) {
+    if (cards[0].dataset.card === cards[1].dataset.card) {
+      console.log('compare is true')
+      //theres a match, add match class to the item.
+      cards[0].classList.add('match');
+      cards[1].classList.add('match');
+      //reset the picked cards array for the next guess.
+      pickedCards = [];
+    } else {
+      setTimeout( () => { 
+        cards[0].classList.remove('show', 'open');
+        cards[1].classList.remove('show', 'open');
+        //reset the picked cards array for the next guess.
+        pickedCards = []
+      }, 1000);
+      
+    };
+    moves++;
+  }
+  
+}
+
 //if they match, assign match class
 //if they don't match, hide them again after some animation and a few seconds
 //only allow two cards to be shown at once
